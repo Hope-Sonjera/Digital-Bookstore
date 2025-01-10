e5f297be3283d1aefeda749d9d46d4b77ddf170a
Hope-Sonjera <hpsonjera@gmail.com>
2025-01-10 08:40:30 +0200

    Initial commit

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const MongoDBURL =
+'mongodb+srv://admin:admin@cluster0.izzymiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
---
+++ Backend/index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import cors from 'cors';
+import { PORT, MongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
+const app = express();
+
+// Use CORS middleware to allow cross-origin requests
+app.use(cors());
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Basic route to check if the server is running
+app.get('/', (request, response) => {
+    console.log('Request received at root');
+    return response.status(200).send('Welcome to MIE Library');
+});
+
+// Route to Save a new book
+app.post('/books', async (request, response) => {
+    try {
+        // Destructure fields from the incoming request body
+        const { title, author, publisherYear } = request.body;
+
+        // Check if all required fields are provided
+        if (!title || !author || !publisherYear) {
+            return response.status(400).send({
+                message: 'Please provide all required fields: title, author, publisherYear',
+            });
+        }
+
+        // Create a new book document with the provided data
+        const newBook = { title, author, publisherYear };
+
+        // Save the new book to the database
+        const book = await Book.create(newBook);
+
+        // Return the newly created book as the response
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// Connect to MongoDB and start the server
+mongoose
+    .connect(MongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        // Start the server once the database connection is successful
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to database:', error);
+    });
